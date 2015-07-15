c87b7fa0e6a196d2239b07eeedbeb189a3e84b33
Piotrek Dubiel <piotr.dubiel@polidea.com>
2015-07-15 10:11:13 +0200

    ?

--- lib/multi_junit/formatter.rb
+++ lib/multi_junit/formatter.rb
@@ -3,12 +3,18 @@ require 'cucumber/formatter/junit'
 module MultiJunit
   class Formatter < Cucumber::Formatter::Junit
     def feature_result_filename(feature_file)
-      File.join(@reportdir, "#{ENV["ADB_DEVICE_ARG"] || "TEST"}-#{basename(feature_file)}.xml")
+      File.join(@reportdir, "#{device_name || "TEST"}-#{basename(feature_file)}.xml")
     end
 
     def feature_name(keyword, name)
       feature_name = super
-      @feature_name = ENV['ADB_DEVICE_ARG'] + "-" + feature_name if ENV['ADB_DEVICE_ARG']
+      @feature_name = device_name + "-" + feature_name if device_name
+    end
+
+    private
+
+    def device_name
+      ENV["DEVICE_NAME"] || ENV["ADB_DEVICE_ARG"] || ENV["DEVICE_TARGET"]
     end
   end
 end
